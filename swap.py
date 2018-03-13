021e3813627facf8602a83a2ce5dd1bc0219bfe2
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-13 13:58:16 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+while true:
+  print("Enter 'X' for exit".)
+  str1=input("Enter first string:")
+  str2=input("Enter second string:")
+  if string1=='X':
+    break
+  else:
+    temp=str1
+    str2=temp
+    print("/n string after swapping:")
+    print("First string:",str1)
+    print("Second string:"str2)

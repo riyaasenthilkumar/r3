def736a255f9e46f3f71531bbcc69d0d71a350ac
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-13 15:57:40 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,13 @@
+def nearlyEqual(str1,str2):
+  str1=input('First word')
+  str2=input('Second word')
+    if str1==str2:
+      print(true)
+    else:
+      print(false)
+   return nearlyEqual(str1,str2)
+   print(nearlyEqual(str1,str2))
+str1=()
+str2=()
+print nearlyEqual(str1,str2)
+  

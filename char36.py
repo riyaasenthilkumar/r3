4830760939a2f8250f091ec4f419e64154375c72
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-16 17:42:39 +0530

    Create char36.py

---
+++ char36.py
@@ -0,0 +1,11 @@
+str=raw_input("Enter str:")
+cnt1=0
+cnt2=0
+for i in str:
+      if(i.isdigit()):
+            cnt1=cnt1+1
+      count2=count2+1
+print("The num of digits is:")
+print(cnt1)
+print("The num of characters is:")
+print(cnt2)

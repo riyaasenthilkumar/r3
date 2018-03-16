7b057a28031628e9a7e5146d52d25c3797610549
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-16 15:44:49 +0530

    Create word.py

---
+++ word.py
@@ -0,0 +1,9 @@
+fname=input("Enter file name:")
+num_words=0
+with open(fname,'r')as f:
+  for line in f:
+    words=line-split()
+    num_words+=len(words)
+print("number of words:")
+print(num_words)
+  

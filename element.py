2f576f20d2137ddcded4c5db4b721b16bb83e5c3
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-16 15:11:50 +0530

    Create element.py

---
+++ element.py
@@ -0,0 +1,10 @@
+def printRepeating(arr, size):
+ 
+    print("Repeating elements are ",end = '')
+    for i in range (0, size):
+        for j in range (i + 1, size):
+            if arr[i] == arr[j]:
+                print(arr[i], end = ' ')
+arr = [4, 2, 4, 5, 2, 3, 1]
+arr_size = len(arr)
+printRepeating(arr, arr_size)

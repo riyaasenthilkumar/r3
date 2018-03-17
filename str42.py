1e38f53cc39d12f8c663d79b7b626c1f5cae772e
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-03-17 14:56:19 +0530

    Create str42.py

---
+++ str42.py
@@ -0,0 +1,11 @@
+while True:
+  print("Enter 'x' for exit")
+  str1=input("Enter string1:")
+  str2=input("Enter string2:")
+  if str1=='x':
+    break
+  else:
+    if str1==str2:
+      print("Both string are Equal.\n")
+    else:
+      print("strings are not equal.\n")

008f97f217ce16538d652e80fa38d8eae60a2fa9
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-04-09 15:22:43 +0530

    Create power70.py

---
+++ power70.py
@@ -0,0 +1,10 @@
+def nextPowerOf2(n):
+    count = 0;
+    if (n and not(n&(n-1))):
+        return n
+    while( n != 0):
+        n >>= 1
+        count += 1
+    return 1 << count;
+n = 0
+print(nextPowerOf2(n))
